fcc08eebf2ab7d16749aae4c0931e807e625e321
vishal <vpatel38@kent.edu>
2019-12-02 19:51:46 -0500

    page

--- microblog/config.py
+++ microblog/config.py
@@ -14,3 +14,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['vishal.hmt@gmail.com']
+    POSTS_PER_PAGE = 3
