f5b72f9820110f793f0111cdfaaef045952abf5e
vishal <vpatel38@kent.edu>
2019-12-02 20:22:07 -0500

    bootstrap

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
+from flask_bootstrap import Bootstrap
 import os
 
 
@@ -15,6 +16,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
